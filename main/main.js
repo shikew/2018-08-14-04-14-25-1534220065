59500ca2ce5a23965552529f31837a8f1edab438
shikew <wangshikee@126.com>
2019-05-28 22:28:58 +0800

    修改 main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+const numbers = [ [ '._.','...','._.','._.','...','._.','._.','._.','._.','._.'],
+['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],
+['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|' ] ]
+
+
+module.exports = function main(str) {
+    const result = [[],[],[]]
+
+    for(let s of str){
+        result[0].push(numbers[0][s])
+        result[1].push(numbers[1][s])
+        result[2].push(numbers[2][s])
+    }
+    return result.map(row => row.join(' ')+ '\n').join('')
 };
